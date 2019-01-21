e1a9f369f1900754476fb5e54741a48af0f77f2a
Wubbadubba <46880067+Wubbadubba@users.noreply.github.com>
2019-01-20 22:41:56 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+/*********************
+* Hello.java
+**********************
+* Hello world/Lab 1 Project
+*_____________________
+* Yousuf Chudhary
+* January 20th
+* CSMC 255 003
+**********************/
+
+public class Hello {
+ public static void printHeading() {
+ System.out.println("Yousuf Chudhary");
+ System.out.println("Hello world");
+ System.out.println("Lab 1 Project");
+ System.out.println("Java SE 8u192");
+ System.out.println("CSMC 255 003");
+ System.out.println();
+}
+
+ public static void main(String [] args) {
+  printHeading();
+  System.out.println("Hello");
+ }
+}
+	
